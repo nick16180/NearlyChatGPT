e1c1abdd5ac364138223aaf287e36594bdc79e2b
nick16180 <nickolas.alvarado@gmail.com>
2023-08-26 21:07:34 -0700

    Add files via upload
    Revised files

--- README.md
+++ README.md
@@ -1,46 +1,10 @@
-# Streamlit ChatGPT Harness
+# Nearly Chat GPT
 
-A fully python based Streamlit development harness for ChatGPT hosted in Azure OpenAI Service.
+This is a fork of the repo [here](https://github.com/microsoft/az-oai-chatgpt-streamlit-harness).
 
-## Getting Started
+My version of this package has a number of modifications:
 
-### Setting up the Environment
-
-To get started, you will need to create a `.env` file in the root of the project.  This file will contain the environment variables needed to run the app. You can make a copy of `local.env` and rename it to `.env` to get started.  You will need to fill in the following values:
-
-```bash
-AZURE_OPENAI_ENDPOINT=""
-AZURE_OPENAI_KEY=""
-AZURE_OPENAI_API_VERSION=""
-AZURE_OPENAI_CHATGPT_DEPLOYMENT=""
-```
-
-You can find the values for these variables in the Azure Portal.  You will need to create an Azure OpenAI resource and deploy the ChatGPT model to it.  Once you have done that, you can find the values for these variables in the resource.
-
-The app will load this .env file using the python-dotenv library.
-
-### Running the App
-
-To run the app, simply run the `streamlit run app.py`.  This will start the app on port 8501.  You can then access the app at `http://localhost:8501`. If running in a container, you will need to forward the port to your local machine if VSCode does not do it for you automatically.
-
-### Devcontainer
-
-This project is designed to be used with VSCode and the Remote Containers extension.  Once you have the extension installed, open the project in VSCode and you will be prompted to open the project in a container.  This will build the container and install all the dependencies.
-
-### Python Dependencies
-
-The `requirements.txt` file contains all the python dependencies for this project.  The `devcontainer.json` file will automatically install these dependencies when the container is built.
-
-## Credits
-
-This project was forked from the [streamlit-chatgpt-ui](https://github.com/marshmellow77/streamlit-chatgpt-ui) under the MIT license and adapted to the Azure OpenAI Service.
-
-## Contributing
-This project welcomes contributions and suggestions. Most contributions require you to agree to a Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
-
-When you submit a pull request, a CLA bot will automatically determine whether you need to provide a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions provided by the bot. You will only need to do this once across all repos using our CLA.
-
-This project has adopted the Microsoft Open Source Code of Conduct. For more information see the Code of Conduct FAQ or contact opencode@microsoft.com with any additional questions or comments.
-
-## Trademarks
-This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft trademarks or logos is subject to and must follow Microsoft's Trademark & Brand Guidelines. Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship. Any use of third-party trademarks or logos are subject to those third-party's policies.
+- Poetry for package management
+- Can use azure or openai APIs
+- UI enhancements
+- Cleaned up files
--- app.py
+++ app.py
@@ -6,38 +6,91 @@ import openai
 import datetime
 import json
 
+import logging
 
-# region ENV + SDK SETUP
+logging.basicConfig(level=logging.WARNING)
 
 # Load environment variables
 ENV = dotenv.dotenv_values(".env")
-with st.sidebar.expander("Environment Variables"):
-    st.write(ENV)
 
 # Set up the Open AI Client
+if ENV["API_TYPE"] == "azure":
+    openai.api_base = ENV["AZURE_OPENAI_ENDPOINT"]
+    openai.api_version = ENV["AZURE_OPENAI_API_VERSION"]
+    openai.api_key = ENV["AZURE_OPENAI_KEY"]
+else:
+    openai.api_key = ENV["OPENAI_API_KEY"]
 
-openai.api_type = "azure"
-openai.api_base = ENV["AZURE_OPENAI_ENDPOINT"]
-openai.api_version = ENV["AZURE_OPENAI_API_VERSION"]
-openai.api_key = ENV["AZURE_OPENAI_KEY"]
-# endregion
 
-# region PROMPT SETUP
+def generate_response(prompt, temperature, topp):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+    response = None
+    try:
+        if ENV["API_TYPE"] == "azure":
+            completion = openai.ChatCompletion.create(
+                engine=ENV["AZURE_OPENAI_CHATGPT_DEPLOYMENT"],
+                messages=st.session_state["messages"],
+                temperature=temperature,
+                top_p=topp,
+            )
+        else:
+            completion = openai.ChatCompletion.create(
+                model=ENV["OPENAI_MODEL"],
+                messages=st.session_state["messages"],
+            )
+        response = completion.choices[0].message.content
+    except openai.error.APIError as e:
+        st.write(response)
+        response = f"The API could not handle this content: {str(e)}"
+    st.session_state["messages"].append({"role": "assistant", "content": response})
 
+    return (
+        response,
+        completion.usage.total_tokens,
+        completion.usage.prompt_tokens,
+        completion.usage.completion_tokens,
+    )
+
+
+def generate_initial_response(prompt):
+    st.session_state["messages"].append({"role": "system", "content": prompt})
+    response = None
+    try:
+        if ENV["API_TYPE"] == "azure":
+            completion = openai.ChatCompletion.create(
+                engine=ENV["AZURE_OPENAI_CHATGPT_DEPLOYMENT"],
+                messages=st.session_state["messages"],
+            )
+        else:
+            completion = openai.ChatCompletion.create(
+                model=ENV["OPENAI_MODEL"],
+                messages=st.session_state["messages"],
+            )
+        response = completion.choices[0].message.content
+    except openai.error.APIError as e:
+        st.write(response)
+        response = f"The API could not handle this content: {str(e)}"
+    st.session_state["messages"].append({"role": "assistant", "content": response})
+
+    return (
+        response,
+        completion.usage.total_tokens,
+        completion.usage.prompt_tokens,
+        completion.usage.completion_tokens,
+    )
+
+
+# PROMPT SETUP
 default_prompt = """
-You are an AI assistant  that helps users write concise\
- reports on sources provided according to a user query.\
- You will provide reasoning for your summaries and deductions by\
- describing your thought process. You will highlight any conflicting\
- information between or within sources. Greet the user by asking\
- what they'd like to investigate.
+You are a helpful assistant. Provide all answers in markdown format.
 """
 
-system_prompt = st.sidebar.text_area("System Prompt", default_prompt, height=200)
-seed_message = {"role": "system", "content": system_prompt}
-# endregion
+seed_message = {"role": "system", "content": default_prompt}
 
-# region SESSION MANAGEMENT
+st.set_page_config(page_title="Nearly ChatGPT", page_icon=":mountain:", layout="wide")
+st.title("Nearly ChatGPT")
+
+# SESSION MANAGEMENT
 # Initialise session state variables
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -51,16 +104,25 @@ if "cost" not in st.session_state:
     st.session_state["cost"] = []
 if "total_tokens" not in st.session_state:
     st.session_state["total_tokens"] = []
+if "prompt_tokens" not in st.session_state:
+    st.session_state["prompt_tokens"] = []
+if "completion_tokens" not in st.session_state:
+    st.session_state["completion_tokens"] = []
 if "total_cost" not in st.session_state:
     st.session_state["total_cost"] = 0.0
-# endregion
+if "temperature" not in st.session_state:
+    st.session_state["temperature"] = 1.0
+if "topp" not in st.session_state:
+    st.session_state["topp"] = 1.0
 
-# region SIDEBAR SETUP
-
+# SIDEBAR SETUP
+st.sidebar.write("Total cost of this conversation")
 counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(
-    f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}"
+counter_placeholder.code(f"${st.session_state['total_cost']:.5f}")
+temperature = st.sidebar.slider(
+    "Temperature", 0.0, 1.0, st.session_state["temperature"]
 )
+topp = st.sidebar.slider("Top P", 0.0, 1.0, st.session_state["topp"])
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
 if clear_button:
@@ -72,45 +134,20 @@ if clear_button:
     st.session_state["cost"] = []
     st.session_state["total_cost"] = 0.0
     st.session_state["total_tokens"] = []
-    counter_placeholder.write(
-        f"Total cost of this conversation: £{st.session_state['total_cost']:.5f}"
-    )
-
+    st.session_state["prompt_tokens"] = []
+    st.session_state["completion_tokens"] = []
+    counter_placeholder.code(f"${st.session_state['total_cost']:.5f}")
 
 download_conversation_button = st.sidebar.download_button(
     "Download Conversation",
     data=json.dumps(st.session_state["messages"]),
-    file_name=f"conversation.json",
+    file_name="conversation.json",
     mime="text/json",
 )
 
-# endregion
-
-
-def generate_response(prompt):
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-    try:
-        completion = openai.ChatCompletion.create(
-            engine=ENV["AZURE_OPENAI_CHATGPT_DEPLOYMENT"],
-            messages=st.session_state["messages"],
-        )
-        response = completion.choices[0].message.content
-    except openai.error.APIError as e:
-        st.write(response)
-        response = f"The API could not handle this content: {str(e)}"
-    st.session_state["messages"].append({"role": "assistant", "content": response})
-
-    # print(st.session_state['messages'])
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
-
-
-st.title("Streamlit ChatGPT Demo")
-
 # container for chat history
 response_container = st.container()
+
 # container for text box
 container = st.container()
 
@@ -119,21 +156,32 @@ with container:
         user_input = st.text_area("You:", key="input", height=100)
         submit_button = st.form_submit_button(label="Send")
 
-    if submit_button and user_input:
-        output, total_tokens, prompt_tokens, completion_tokens = generate_response(
-            user_input
-        )
-        st.session_state["past"].append(user_input)
-        st.session_state["generated"].append(output)
-        st.session_state["model_name"].append(ENV["AZURE_OPENAI_CHATGPT_DEPLOYMENT"])
-        st.session_state["total_tokens"].append(total_tokens)
+    with st.spinner("Talking with AI..."):
+        if submit_button and user_input:
+            output, total_tokens, prompt_tokens, completion_tokens = generate_response(
+                user_input, temperature, topp
+            )
+            st.session_state["past"].append(user_input)
+            st.session_state["generated"].append(output)
+            if ENV["API_TYPE"] == "azure":
+                st.session_state["model_name"].append(
+                    ENV["AZURE_OPENAI_CHATGPT_DEPLOYMENT"]
+                )
+            else:
+                st.session_state["model_name"].append(ENV["OPENAI_MODEL"])
+            st.session_state["total_tokens"].append(total_tokens)
+            st.session_state["prompt_tokens"].append(prompt_tokens)
+            st.session_state["completion_tokens"].append(completion_tokens)
 
-        # from https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/#pricing
-        cost = total_tokens * 0.001625 / 1000
+            if ENV["API_TYPE"] == "azure":
+                # from https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/#pricing
+                cost = prompt_tokens * 0.0015 / 1000 + completion_tokens * 0.002 / 1000
+            else:
+                # https://openai.com/pricing
+                cost = prompt_tokens * 0.0015 / 1000 + completion_tokens * 0.002 / 1000
 
-        st.session_state["cost"].append(cost)
-        st.session_state["total_cost"] += cost
-
+            st.session_state["cost"].append(cost)
+            st.session_state["total_cost"] += cost
 
 if st.session_state["generated"]:
     with response_container:
@@ -142,11 +190,6 @@ if st.session_state["generated"]:
                 st.session_state["past"][i],
                 is_user=True,
                 key=str(i) + "_user",
-                avatar_style="shapes",
             )
-            message(
-                st.session_state["generated"][i], key=str(i), avatar_style="identicon"
-            )
-        counter_placeholder.write(
-            f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}"
-        )
+            message(st.session_state["generated"][i], key=str(i))
+        counter_placeholder.code(f"${st.session_state['total_cost']:.5f}")
